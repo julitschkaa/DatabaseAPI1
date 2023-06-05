0aac18eddf9eb83977344152f46475f388e8d29f
julitschkaa <schaak@posteo.net>
2023-06-05 18:47:05 +0200

    added support for seperate binary api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Union, List
+from typing import Union, List, Any
 
 import uvicorn
 import http3
@@ -8,6 +8,7 @@ from uuid import uuid4
 
 from fastapi import FastAPI, status
 from fastapi_sqlalchemy import DBSessionMiddleware, db
+from requests import Response
 
 from Datafile_API.fastq_parser import get_fastq_metrics
 from Datafile_API.sam_parser import get_sam_metrics
@@ -35,33 +36,79 @@ app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/binary_API_sam_json_from_fastq/")
+async def call_binary_api_sam_json_from_fastq(fastqpath: Union[str],
+                                              fastaurl: Union[str],
+                                              rootpath: Union[str]):
+    response = await call_binary_api(endpoint="/sam_json_from_fastq/", params={"fastqpath":fastqpath,
+                                                                                "fastaurl":fastaurl,
+                                                                                "rootpath":rootpath})
+    return response.json()
 
-@app.get("/binary_api/") #TODO exchange filepath for params dict
-async def call_binary_api(endpoint: Union[str], filepath: Union[str]):
+@app.get("/binary_API_sam_json/")  # TODO write json to db
+async def call_binary_api_sam_sjon_endpoint(sampath: Union[str]):
+    response = await call_binary_api(endpoint="/sam_json/", params={"sampath": sampath})
+    return response.json()
+
+@app.get("/binary_API_sam_csv/")
+async def call_binary_api_sam_csv_endpoint(sampath: Union[str]):
+    response = await call_binary_api(endpoint="/sam_csv/", params={"sampath": sampath})
+    return response.json()
+
+@app.get("/binary_API_fastq_json/")
+async def call_binary_api_fastq_json_endpoint(fastqpath: Union[str]):
+    response = await call_binary_api(endpoint="/fastq_json/", params={"fastqpath": fastqpath})
+    return response.json()
+
+@app.get("/binary_API_fastq_csv/")
+async def call_binary_api_fastq_csv_endpoint(fastqpath: Union[str]):
+    response = await call_binary_api(endpoint="/fastq_csv/", params={"fastqpath": fastqpath})
+    return response.json()
+
+@app.get("/binary_API_kraken_json/")
+async def call_binary_api_kraken_json_endpoint(krakendb: Union[str],
+                                               fastafile:Union[str],
+                                               output: Union[str]):
+    response = await call_binary_api(endpoint="/kraken_json/", params={"krakendb": krakendb,
+                                                                       "fastafile": fastafile,
+                                                                       "output": output})
+    return response.json()
+
+@app.get("/binary_API_init/")
+async def call_binary_api_init_by_config_obj(config: Union[str]):
+    response = await call_binary_api(endpoint="/init/", params={"config": config})
+    return response.json()
+
+@app.get("/binary_API_init_by_path/")
+async def call_binary_api_init_by_path(configpath: Union[str]):
+    response = await call_binary_api(endpoint="/init_by_path/", params={"config":configpath})
+    return response.json()
+
+@app.get("/binary_API_init_async/")
+async def call_binary_api_init_async(configpath: Union[str], threadname: Union[str]):
+    response = await call_binary_api(endpoint="/init_async/", params={"config":configpath, "threadname": threadname})
+    return response.json()
+
+@app.get("/binary_API_check_thread/")
+async def call_binary_api_check_thread(threadname: Union[str]):
+    response = await call_binary_api(endpoint="/check_thread/", params={"threadname": threadname})
+    return response.json()
+
+@app.get("/binary_API_json_from_bowtie_exec/")
+async def call_binary_api_json_from_bowtie_execution(fastapath: Union[str], fastqpath: Union[str], rootpath: Union[str]):
+    response = await call_binary_api(endpoint="/sam_json_from_bowtie_execution/", params={"fastapath":fastapath,
+                                                                                          "fastqpath": fastqpath,
+                                                                                          "rootpath": rootpath})
+    return response.json()
+
+@app.get("/binary_api/")
+async def call_binary_api(endpoint: Union[str], params: dict[str, str]):
     binary_api_url = os.environ['BINARY_API_URL']
-    r = requests.get(url=binary_api_url + endpoint, params={"config": filepath})
-    return r.json()
+    response = requests.get(url=binary_api_url + endpoint, params=params)
+    return response
 
 
-@app.post("/fastqlukas/")
-async def fastq(filepath: Union[str]):
-    db_file_name_and_uuid = ModelFile_name_and_uuid(file_name=filepath,
-                                                    file_uuid=uuid4())
-    db.session.add(db_file_name_and_uuid)
-    db.session.commit()
-    fastq_id = db_file_name_and_uuid.id
-
-    readsjson = call_binary_api("fastq_json/", filepath)
-
-    for read in readsjson:
-        db_raw_data = ModelRaw_data(sequence_id=read["id"], sequence=str(read["sequence"]),
-                                    phred_quality=read["phred_quality"], file_id=fastq_id)
-        db.session.add(db_raw_data)
-        db.session.commit()
-    return {"added %d reads to postgresdb", len(readsjson)}
-
-
-@app.post('/raw_data/', response_description="write entries to raw data table" ,
+@app.post('/raw_data/', response_description="write entries to raw data table",
           response_model=SchemaRaw_data, status_code=status.HTTP_201_CREATED)
 async def post_raw_data(raw_data: SchemaRaw_data):
     db_raw_data = ModelRaw_data(sequence_id=raw_data.sequence_id,
@@ -78,7 +125,7 @@ async def post_raw_data(raw_data: SchemaRaw_data):
 
 
 @app.post('/binary_result/', response_description="write entries to binary_result table"
-    ,response_model=SchemaBinary_result, status_code=status.HTTP_201_CREATED)
+    , response_model=SchemaBinary_result, status_code=status.HTTP_201_CREATED)
 async def post_binary_result(binary_result: SchemaBinary_result):
     db_binary_result = ModelRaw_data(sequence_id=binary_result.sequence_id,
                                      type=binary_result.type,
@@ -92,44 +139,48 @@ async def post_binary_result(binary_result: SchemaBinary_result):
 
 @app.get('/sequence_id/', response_description="get read including all binary entries by sequence_id")
 async def get_entries_by_sequence_id(sequence_id: Union[str]):
-    raw_data = db.session.query(ModelRaw_data).all()#TODO join tables on sequence id
+    raw_data = db.session.query(ModelRaw_data).all()  # TODO join tables on sequence id
     raw_data_results = [read for read in raw_data if read.sequence_id == sequence_id]
-    if len(raw_data_results)==0:
+    if len(raw_data_results) == 0:
         return status.HTTP_404_NOT_FOUND
     binary_data = db.session.query(ModelBinary_results).all()
     binary_results = [entry for entry in binary_data if entry.sequence_id == sequence_id]
     combined_results = {}
-    if len(binary_results)>0:
+    if len(binary_results) > 0:
         for read in raw_data_results:
-            combined_results[read]=binary_results
+            combined_results[read] = binary_results
 
-    #jointtables = db.session.query(ModelRaw_data).outerjoin(ModelBinary_results, ModelRaw_data.sequence_id==ModelBinary_results.sequence_id) #it says first here because "maximum recursion  depth exceeded error" otherwise
+    # jointtables = db.session.query(ModelRaw_data).outerjoin(ModelBinary_results, ModelRaw_data.sequence_id==ModelBinary_results.sequence_id) #it says first here because "maximum recursion  depth exceeded error" otherwise
     # results = db.session.query(ModelRaw_data).filter(ModelRaw_data.sequence_id==dsequence_id).first() #it says first here because "maximum recursion  depth exceeded error" otherwise
 
     return raw_data_results, binary_results
 
+
 @app.get('/raw_data/', response_description="list all reads in raw data table", response_model=List[SchemaRaw_data])
 async def list_raw_data_entries():
-    raw_data = db.session.query(ModelRaw_data).all() #too many results
-    #raw_data = db.session.query(ModelRaw_data).first()  # just here because i needed a random sequence id
+    raw_data = db.session.query(ModelRaw_data).all()  # too many results
+    # raw_data = db.session.query(ModelRaw_data).first()  # just here because i needed a random sequence id
     # raw_data = db.session.query(ModelRaw_data).count()
     return raw_data
 
 
-@app.get('/binary_results_by_seq_id/', response_description="list all reads with matching seq_id in binary_results table", response_model=List[SchemaBinary_result])
+@app.get('/binary_results_by_seq_id/',
+         response_description="list all reads with matching seq_id in binary_results table",
+         response_model=List[SchemaBinary_result])
 async def list_binary_results(sequence_id: Union[str]):
     binary_results = db.session.query(ModelBinary_results).all()
     results = [x for x in binary_results if x.sequence_id == sequence_id]
     return results
 
 
-@app.get('/file_name_and_uuid/', response_description="list all entries in file_name_and_id", response_model=List[SchemaFile_name_and_uuid])
+@app.get('/file_name_and_uuid/', response_description="list all entries in file_name_and_id",
+         response_model=List[SchemaFile_name_and_uuid])
 async def list_file_name_and_uuid():
     file_name_and_uuids = db.session.query(ModelFile_name_and_uuid).all()
     return file_name_and_uuids
 
 
-@app.post('/file_name_and_uuid/', response_description="write entries to file_name_and_id table" ,
+@app.post('/file_name_and_uuid/', response_description="write entries to file_name_and_id table",
           response_model=SchemaFile_name_and_uuid, status_code=status.HTTP_201_CREATED)
 async def create_file_name_and_uuid_entries(file_name_and_uuid: SchemaFile_name_and_uuid):
     db_file_name_and_uuid = ModelFile_name_and_uuid(file_name=file_name_and_uuid.name,
@@ -139,7 +190,7 @@ async def create_file_name_and_uuid_entries(file_name_and_uuid: SchemaFile_name_
     return db_file_name_and_uuid
 
 
-@app.post('/fastq/', status_code=status.HTTP_201_CREATED)#TODO: check if file is already in filename_and_uuid_table
+@app.post('/fastq/', status_code=status.HTTP_201_CREATED)  # TODO: check if file is already in filename_and_uuid_table
 async def create_fastq_entries(filepath: Union[str]):
     db_file_name_and_uuid = ModelFile_name_and_uuid(file_name=filepath,
                                                     file_uuid=uuid4())
@@ -160,10 +211,10 @@ async def create_fastq_entries(filepath: Union[str]):
                                     file_id=fastq_id)
         db.session.add(db_raw_data)
         db.session.commit()
-    return {"added "+str(len(reads))+" reads to postgresDB"}
+    return {"added " + str(len(reads)) + " reads to postgresDB"}
 
 
-@app.post('/sam/', status_code=status.HTTP_201_CREATED)#TODO: check if file is already in filename_and_uuid_table
+@app.post('/sam/', status_code=status.HTTP_201_CREATED)  # TODO: check if file is already in filename_and_uuid_table
 async def create_sam_enries(filepath: Union[str]):
     binary_results = get_sam_metrics(filepath)
 
@@ -204,10 +255,10 @@ async def create_sam_enries(filepath: Union[str]):
             db.session.add(db_binary_results)
             db.session.commit()
             entry_count += 1
-    return {"added "+str(entry_count)+" entries from sam file to postgresdb"}
+    return {"added " + str(entry_count) + " entries from sam file to postgresdb"}
 
 
-@app.post('/kraken2/', status_code=status.HTTP_201_CREATED)#TODO: check if file is already in filename_and_uuid_table
+@app.post('/kraken2/', status_code=status.HTTP_201_CREATED)  # TODO: check if file is already in filename_and_uuid_table
 async def create_kraken_entries(filepath: Union[str]):
     kraken_results = get_kraken_metrics(filepath)
 
