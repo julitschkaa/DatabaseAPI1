0112d4a5b9e64623450bf61e07abcea455c88de4
julitschkaa <schaak@posteo.net>
2023-07-17 17:47:47 +0200

    refactored fastq_parser. removed unused imports

--- Datafile_API/fastq_parser.py
+++ Datafile_API/fastq_parser.py
@@ -1,11 +1,4 @@
-import os
-import math
-import zlib
 from Bio import SeqIO
-import pylab as plt
-import numpy as np
-import pandas as pd
-import matplotlib.patches as patches
 
 def get_fastq_metrics(filename):
     fastq_parser = SeqIO.parse(open(filename, "rt"), "fastq")
@@ -17,9 +10,10 @@ def get_fastq_metrics(filename):
         read["sequence_length"] = len(str(record.seq))
         read["min_quality"] = min(record.letter_annotations["phred_quality"])
         read["max_quality"] = max(record.letter_annotations["phred_quality"])
-        read["average_quality"] = sum(record.letter_annotations["phred_quality"]) / len(record.letter_annotations["phred_quality"])
+        read["average_quality"] = sum(record.letter_annotations["phred_quality"]) \
+                                  / len(record.letter_annotations["phred_quality"])
         read["phred_quality"] = record.letter_annotations["phred_quality"]
         reads.append(read)
-        if(len(reads)==20):
-            break
+        #if(len(reads)==20): #input limit for test purpose
+            #break
     return reads
